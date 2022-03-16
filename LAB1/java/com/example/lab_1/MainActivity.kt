a73e39cd7626213f471e18e117fd87b2128dd4b6
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2022-03-16 21:26:03 +0100

    hotfix

--- LAB1/java/com/example/lab_1/MainActivity.kt
+++ LAB1/java/com/example/lab_1/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.lab_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,41 +17,39 @@ class MainActivity : AppCompatActivity() {
         var button2: Button = findViewById(R.id.button2)
         var progressBar: ProgressBar = findViewById(R.id.progressBar)
         var start = false
-        var progress = 0
-        var i= 0
-        val handler = Handler()
+        var threadCreated = false
+        var finished = false
 
-        button1.setOnClickListener { textView.text = "Breaking the RSA keys..."
-            i = progressBar.progress
+        button1.setOnClickListener {
             start = true
-            Thread(Runnable {
-                // this loop will run until the value of i becomes 99
-                while (i < 100) {
-                    if (start == true)
-                    {
-                        i += 1
-                        progressBar.progress = i
+            if (!finished) {
+                textView.text = "Breaking the RSA keys..."
+            }
+            var i = progressBar.progress
+            if (!threadCreated) {
+                threadCreated = true
+                Thread(Runnable {
+                    while (i < 100) {
+                        if (start) {
+                            i += 1
+                            progressBar.progress = i
+                        }
+                        try {
+                            Thread.sleep(600)
+                        } catch (e: InterruptedException) {
+                            e.printStackTrace()
+                        }
                     }
-                    try {
-                        Thread.sleep(800)
-                    } catch (e: InterruptedException) {
-                        e.printStackTrace()
-                    }
-                }
-
-                // setting the visibility of the progressbar to invisible
-                // or you can use View.GONE instead of invisible
-                // View.GONE will remove the progressbar
-                textView.text = "Calculations completed"
-
-            }).start()
-
-
+                    textView.text = "Calculations completed"
+                    finished = true
+                }).start()
+            }
         }
         button2.setOnClickListener {
             start = false
-            textView.text = "Calculations stopped"}
-
+            if (!finished){
+                textView.text = "Calculations stopped"
+            }
+        }
     }
-
 }
